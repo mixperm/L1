4a8fe037f2b4dca79bf7f213650cbb117cd111f7
Максим Шадрин <mshadrin@mail.ru>
2021-01-13 10:35:12 +0500

    test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            a = 0;
+            Console.WriteLine("hello!");
+            Console.ReadLine();
+        }
+    }
+}
